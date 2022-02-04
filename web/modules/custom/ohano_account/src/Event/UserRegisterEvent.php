95a82d2ebb84e48548343d72cf3dc9a8ae795c28
Thomas Artmann <thwohmas@gmail.com>
2022-02-04 05:19:41 +0100

    #15: Changed type of $user to be a Drupal User object

--- web/modules/custom/ohano_account/src/Event/UserRegisterEvent.php
+++ web/modules/custom/ohano_account/src/Event/UserRegisterEvent.php
@@ -3,12 +3,13 @@
 namespace Drupal\ohano_account\Event;
 
 use Drupal\Component\EventDispatcher\Event;
+use Drupal\Core\Session\AccountInterface;
 
 class UserRegisterEvent extends Event {
 
   const EVENT_NAME = 'ohano_account_user_register';
 
-  public function __construct(public array $userData) {
+  public function __construct(public AccountInterface $user) {
 
   }
 
