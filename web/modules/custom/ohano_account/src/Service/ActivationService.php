1afe219b2199fd03f7849929fc73159e23cb7390
Thomas Artmann <thwohmas@gmail.com>
2022-02-07 04:14:49 +0100

    #15: Created service class for account activations

---
+++ web/modules/custom/ohano_account/src/Service/ActivationService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\ohano_account\Service;
+
+use Drupal\ohano_account\Entity\AccountActivation;
+
+class ActivationService {
+
+  public function verifyActivation(AccountActivation $activation): bool {
+    
+  }
+
+}
