36c0b82986bd52c726cd33d38c2c8cbd1467a14e
Thomas Artmann <thwohmas@gmail.com>
2022-02-07 04:11:21 +0100

    #15: Completed activation controller

--- web/modules/custom/ohano_account/src/Controller/ActivationController.php
+++ web/modules/custom/ohano_account/src/Controller/ActivationController.php
@@ -4,29 +4,48 @@ namespace Drupal\ohano_account\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\Core\Entity\EntityStorageException;
 use Drupal\ohano_account\Entity\AccountActivation;
+use Drupal\user\Entity\User;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class ActivationController extends ControllerBase {
 
   public function activateAccount(string $code) {
-    if (\Drupal::currentUser()->isAnonymous()) {
-      $path = '/user/login?destination=/account/activate/' . $code;
-      \Drupal::messenger()->addError($this->t('You need to be logged in to activate your account.'));
-      return new RedirectResponse($path);
-    }
-
     $activation = \Drupal::entityQuery('account_activation')
-      ->condition('username', \Drupal::currentUser()->getAccountName())
+      ->condition('code', $code)
       ->execute();
 
     $activation = AccountActivation::load(array_values($activation)[0]);
-    if ($code == $activation->getCode()) {
+    if ($code == $activation->getCode() && $activation->getActivatedOn() < 1000) {
+      $username = $activation->getUsername();
+      $email = $activation->getEmail();
+      $userQuery = \Drupal::entityQuery('user')
+        ->condition('name', $username)
+        ->condition('mail', $email)
+        ->execute();
+      $user = User::load(array_values($userQuery)[0]);
+
+      if (empty($user)) {
+        \Drupal::messenger()->addError($this->t('Something went wrong while activating your account.'));
+        \Drupal::logger('ohano_account')->critical("User entity could not be loaded. Username: '$username', email: '$email', code: '$code'");
+        return new RedirectResponse('/');
+      }
+
+      $user->activate();
+      try {
+        $user->save();
+      } catch (EntityStorageException $e) {
+        \Drupal::messenger()->addError($this->t('Something went wrong while activating your account.'));
+        \Drupal::logger('ohano_account')->critical($e->getMessage());
+        return new RedirectResponse('/');
+      }
+
       $activation->setActivatedOn(new DrupalDateTime());
       $activation->setIsValid(TRUE);
       $activation->save();
 
-      \Drupal::messenger()->addError($this->t('Your account has been activated successfully. You can now continue.'));
+      \Drupal::messenger()->addMessage($this->t("Your account has been activated successfully. You can now continue."));
 
       $path = '/';
     } else {
