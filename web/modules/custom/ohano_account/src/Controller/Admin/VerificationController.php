fcb6adb7cfd5c5cdc7cbdbc3af589d6a1128c5e8
Thomas Artmann <artmann@die-etagen.de>
2022-02-09 19:37:47 +0100

    #16: Implemented Verification list route

---
+++ web/modules/custom/ohano_account/src/Controller/Admin/VerificationController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\ohano_account\Controller\Admin;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\ohano_account\Entity\AccountVerification;
+
+class VerificationController extends ControllerBase {
+
+  public function index() {
+    $verificationIds = $this->entityTypeManager()
+      ->getStorage('account_verification')
+      ->getQuery()
+//      ->condition('verified', 'null')
+      ->sort('created', 'ASC')
+      ->sort('verified', 'DESC')
+      ->execute();
+    $verificationIds = array_values($verificationIds);
+    /** @var \Drupal\ohano_account\Entity\AccountVerification[] $verifications */
+    $verifications = AccountVerification::loadMultiple($verificationIds);
+
+    $renderedVerifications = [];
+    foreach ($verifications as $verification) {
+      $renderedVerifications[] = $verification->render();
+    }
+
+    #dd($renderedVerifications);
+
+    return [
+      '#theme' => 'account_verification_list',
+      '#verifications' => $renderedVerifications,
+    ];
+  }
+
+}
