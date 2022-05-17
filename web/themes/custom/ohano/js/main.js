bacd3cbc630089c30d01119ddbe50b8ca89cd4d4
Thomas Artmann <artmann@die-etagen.de>
2022-05-17 14:40:36 +0200

    Exported database

--- web/themes/custom/ohano/js/main.js
+++ web/themes/custom/ohano/js/main.js
@@ -1,3 +1,18 @@
 (function ($, Drupal, drupalSettings) {
+  function urlify(text) {
+    let urlRegex = /([^"])(https?:\/\/\S+)([^"])/g;
+    return text.replace(urlRegex, '<a href="$2">$2</a>')
+    // or alternatively
+    // return text.replace(urlRegex, '<a href="$1">$1</a>')
+  }
+
+  Drupal.behaviors.processTextFieldLinks = {
+    attach: function (context, settings) {
+      $(document).once('loaded').ready(function () {
+        let content = $('.node-type--update div');
+        //content.text(urlify(content.text()));
+      });
+    }
+  }
 
 })(jQuery, Drupal, drupalSettings);
